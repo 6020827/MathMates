33540b4b6002eb5a7bfa86054063d027771ebbc2
Chloe7725 <6019146@mborijnland.nl>
2022-03-22 15:38:20 +0100

    Js machtsverheffen

---
+++ Website/JS/machtsverheffen.js
@@ -0,0 +1,4 @@
+function myFunction(){
+    machtsverheffen.antwoord.value = machtsverheffen.num1.value + "**" + machtsverheffen.num2.value;
+    machtsverheffen.antwoord.value = eval(machtsverheffen.antwoord.value);
+} 
