238707e286b498014db8a519703a6ff53dc7aba4
Chloe7725 <6019146@mborijnland.nl>
2022-03-22 13:51:06 +0100

    Js optellen +

--- Website/JS/optellen.js
+++ Website/JS/optellen.js
@@ -1,4 +1,4 @@
 function myFunction(){
-    optellen.antwoord.value = optellen.num1.value + "**" + optellen.num2.value;
+    optellen.antwoord.value = optellen.num1.value + "+" + optellen.num2.value;
     optellen.antwoord.value = eval(optellen.antwoord.value);
 } 
